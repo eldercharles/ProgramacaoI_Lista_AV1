f52df61281e4bd35c1a23f210e506ac5a9e0f9f2
aluno <aluno@mscgresl00416.CGR.ACAD>
2017-04-19 22:29:06 -0300

    Exercicio tres teste

---
+++ src/questoes/exercicio/um/ExercicioTres.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questoes.exercicio.um;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioTres {
+    String teste;
+}
