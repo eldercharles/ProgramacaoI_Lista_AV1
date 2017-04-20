a0970d3ebb0bb5453513750cd9c56734a9e01a38
aluno <aluno@mscgresl00416.CGR.ACAD>
2017-04-19 22:27:12 -0300

    Correção do repositorio (exercicios 1 e 2)

---
+++ src/questoes/exercicio/um/ExercicioDois.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questoes.exercicio.um;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioDois {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println (.1 + .2 == .3);
+       
+       // Porque ele testa a condição, soma e verifica que é false.//
+    }
+}
---
+++ src/questoes/exercicio/um/ExercicioUm.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package questoes.exercicio.um;
+
+/**
+ *
+ * @author aluno
+ */
+public class ExercicioUm {
+
+    public static void main(String[] args) {
+        int suaidade;
+        // int para numeros inteiros.
+        String seunome;
+        // para nomes e usado string.
+        double seupeso;
+        //como e relação a peso precisa ser double.
+        String tiposanguineo;
+        // guardar tipo sanquineo necessita de uma string.
+        int aniversario;
+        // guardao numero inteiro.
+        int calça;
+        // guardar 1 numero inteiro;
+        char sanguineo;
+        sanguineo = 'S';
+        // guardar 1 caracter;
+        boolean casado;
+        // condicional sim ou nao , true ou false;
+        casado = false;
+    }
+}
